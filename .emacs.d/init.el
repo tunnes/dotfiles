372b475bd7452e60cbd594cf5a450c475a5663bb
Ayrton Felipe <ayrton.tunes@outlook.com>
2020-07-07 08:55:55 -0300

    Install projectile package to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,6 +3,17 @@
              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
 
+;; Projectile
+;; An package to enable projec navitation
+;; https://github.com/bbatsov/projectile
+(unless (package-installed-p 'projectile)
+  (package-install 'projectile))
+
+(require 'projectile)
+(define-key projectile-mode-map (kbd "M-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(projectile-mode +1)
+
 ;; Clojure mode
 ;; Enables to work with clojure script language
 ;; https://github.com/clojure-emacs/clojure-mode
@@ -16,17 +27,19 @@
   (package-install 'cider))
 
 (custom-set-variables
- ;; Default charactere space of line numbers bar:
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(display-line-numbers-width 2)
-
- ;; Enables line number:
  '(global-display-line-numbers-mode t)
-
- ;; Enable code hightlight to Clojure Script:
  '(package-selected-packages (quote (clojure-mode))))
 
 (custom-set-faces
- ;; Customization of current line on line numbers bar:
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(line-number-current-line ((t (:weight extra-bold)))))
 
 ;; Enables the line number:
