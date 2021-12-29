db08f088a5c5a09d1d4e9c62dbe8157d28591692
tunnes <ayrton.tunes@outlook.com>
2021-12-29 00:53:58 -0300

    Refactoring emacs setup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,24 +1,28 @@
+;;; package --- Summary
+;;; Commentary:
+(global-set-key (kbd "s-<right>") 'move-end-of-line)
 (require 'package)
+;;; Code:
 (add-to-list 'package-archives
              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
 
-;; Projectile ------------------------------------------------------------------------------
-;; An package to enable projec navitation
+;; Projectile ---------------------------------------------------------------------------------------------
+;; It is a project interaction library for Emacs. Its goal is to provide a nice set of features operating on
+;; a project level without introducing external dependencies.
 ;; https://github.com/bbatsov/projectile
 (unless (package-installed-p 'projectile)
   (package-install 'projectile))
 
 (require 'projectile)
 (define-key projectile-mode-map (kbd "M-p") 'projectile-command-map)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 (projectile-mode +1)
 
 (setq projectile-project-search-path '("~/dev/"))
 
-;; Counsel
-;; [WIP] https://github.com/abo-abo/swiper#ivy
-;; An package that makes more visual nice the mini buffer interactions
+;; Swiper --------------------------------------------------------------------------------------------------
+;; An package that enables a autocompletion aka selection box feature on the mini buffer.
+;; https://github.com/abo-abo/swiper
 (unless (package-installed-p 'counsel)
   (package-install 'counsel))
 
@@ -26,124 +30,127 @@
 
 (setq projectile-completion-system 'ivy)
 
-;; Clojure mode
-
-;; Enables to work with clojure script language
+;; Clojure Mode -------------------------------------------------------------------------------------------
+;; Enables to work with clojure script language.
 ;; https://github.com/clojure-emacs/clojure-mode
 (unless (package-installed-p 'clojure-mode)
   (package-install 'clojure-mode))
 
-;; Cider -----------------------------------------------------------------------------------
-;; It is a interactive tool to work with nREPL of Clojure
+;; Cider --------------------------------------------------------------------------------------------------
+;; It is a interactive tool to work with nREPL of Clojure.
 ;; https://github.com/clojure-emacs/cider
 (unless (package-installed-p 'cider)
   (package-install 'cider))
 
-;; Markdown --------------------------------------------------------------------------------
-;; Enables markdown edition via major mode
+;; Markdown -----------------------------------------------------------------------------------------------
+;; Enables markdown edition via major mode.
 ;; https://github.com/jrblevin/markdown-mode
 (unless (package-installed-p 'markdown-mode)
   (package-install 'markdown-mode))
 
-;; Dracula Theme  --------------------------------------------------------------------------------
-;; Enables emacs dracula theme
-;; https://github.com/dracula/emacs/tree/e5cbc350f49a30ad78a84e68b4e1682d018c9b7f
-(unless (package-installed-p 'dracula-theme)
-  (package-install 'dracula-theme))
-
-(load-theme 'dracula t)
-
-
-;; Clojure Refactor ---------------------------------------------------------------------------
-(unless (package-installed-p 'clj-refactor)
-  (package-install 'clj-refactor))
-
-(require 'clj-refactor)
-
-(defun my-clojure-mode-hook ()
-    (clj-refactor-mode 1)
-    (yas-minor-mode 1) ; for adding require/use/import statements
-    ;; This choice of keybinding leaves cider-macroexpand-1 unbound
-    (cljr-add-keybindings-with-prefix "C-c C-m"))
-
-(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
-
-;; Rainbow-delimiters ----------------------------------------------------------------------------------------------
-
+;; Rainbow Delimiters -------------------------------------------------------------------------------------
+;; It Highlights delimiters such as parentheses, brackets or braces according to their depth, each
+;; successive level is highlighted in a different color.
+;; https://github.com/Fanael/rainbow-delimiters
 (unless (package-installed-p 'rainbow-delimiters)
   (package-install 'rainbow-delimiters))
 
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 
-;; ----------------------------------------------------------------------------------------------
+;; LSP Mode -----------------------------------------------------------------------------------------------
+;; Notice that to work with Ruby I've need to ensure the installation of solargraph gem to the current
+;; version of the project.
+;; https://github.com/emacs-lsp/lsp-mode
+(unless (package-installed-p 'lsp-mode)
+  (package-install 'lsp-mode))
 
+(require 'lsp-mode)
+
+(add-hook 'ruby-mode-hook #'lsp)
+
+;; Changing prefix for lsp-mode keybindings.
+(setq lsp-keymap-prefix "C-c")
+
+;; Flycheck Mode ------------------------------------------------------------------------------------------
+;; Modern on-the-fly syntax checking extension for GNU Emacs.
+;; https://github.com/flycheck/flycheck
+(unless (package-installed-p 'flycheck)
+  (package-install 'flycheck))
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; Company Mode -------------------------------------------------------------------------------------------
+;; Company is a text completion framework for Emacs. The name stands for "complete anything". It uses
+;; pluggable back-ends and front-ends to retrieve and display completion candidates.
+;; https://github.com/company-mode/company-mode
+(unless (package-installed-p 'company)
+  (package-install 'company))
+
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Inf Ruby -------------------------------------------------------------------------------------------
+;; Provides a REPL buffer connected to a Ruby subprocess.
+;; https://github.com/nonsequitur/inf-ruby
+(unless (package-installed-p 'inf-ruby)
+  (package-install 'inf-ruby))
+
+;; RSpec Mode -------------------------------------------------------------------------------------------
+;; RSpec mode provides some convenience functions for dealing with RSpec.
+;; https://github.com/pezra/rspec-mode
+(unless (package-installed-p 'inf-ruby)
+  (package-install 'inf-ruby))
+
+(add-hook 'after-init-hook 'inf-ruby-switch-setup)
+
+;; PbCopy Trick/Workaround --------------------------------------------------------------------------------
+;; An trick using pbcopy to be able to copy the code from terminal into the OS pasteboard, running emacs on
+;; the terminal.
+(defun pt-pbcopy ()
+  "Copy region to pasteboard."
+  (interactive)
+  (print (mark))
+  (when mark-active
+    (shell-command-on-region (point) (mark) "pbcopy")
+    (kill-ring-save (point) (mark))
+    (keyboard-quit)
+    (kill-buffer "Copied!")))
+
+(global-set-key (kbd "M-w") 'pt-pbcopy)
+
+;; Dracula Theme ------------------------------------------------------------------------------------------
+;; Enables emacs dracula theme.
+;; https://github.com/dracula/emacs
+(unless (package-installed-p 'dracula-theme)
+  (package-install 'dracula-theme))
+
+(load-theme 'dracula t)
+
+;; Configuration Stuff ------------------------------------------------------------------------------------
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(display-line-numbers-width 2)
- '(global-display-line-numbers-mode t)
- '(package-selected-packages (quote (plantuml-mode clojure-mode))))
+ '(global-display-line-numbers-mode t))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(line-number ((t (:background "#262626" :foreground "white" :slant italic))))
+ '(line-number ((t (:background "#262626" :foreground "white"))))
  '(line-number-current-line ((t (:background "color-234" :weight ultra-bold))))
  '(mode-line ((t (:background "#767676")))))
+
+;;(add-to-list 'default-frame-alist '(foreground-color . "#22242e"))
+;;(add-to-list 'default-frame-alist '(background-color . "#22242E"))
 
 ;; Enables the line number:
 (global-display-line-numbers-mode)
 
-
-(defun pt-pbpaste ()
-  "Paste data from pasteboard."
-  (interactive)
-  (shell-command-on-region
-   (point)
-   (if mark-active (mark) (point))
-   "pbpaste" nil t))
-
-(defun pt-pbcopy ()
-  "Copy region to pasteboard."
-  (interactive)
-  (print (mark))
-  (when mark-active
-    (shell-command-on-region
-     (point) (mark) "pbcopy")
-     (kill-ring-save (point) (mark))
-     (keyboard-quit)
-     (kill-buffer "Copied!")
-     ))
-
-(global-set-key [?\C-x ?\C-y] 'pt-pbpaste)
-(global-set-key [?\M-w] 'pt-pbcopy)
-
 (setq backup-directory-alist `(("." . "~/.saves")))
 
-
-;; Multiple cursor package ====================================
-;; An package to add multiple cursor mode to emacs
-;; https://github.com/magnars/multiple-cursors.el
-(unless (package-installed-p 'multiple-cursors)
-  (package-install 'multiple-cursors))
-
-(require 'multiple-cursors)
-
-
-(setq mac-command-modifier 'super)
-
-(defun seila ()
-  (kill-buffer "Seila!"))
-
-;(global-set-key [?\C-S-b] 'seila)
- 
-(global-set-key (kbd "C-q") 'mc/edit-lines)
-
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(provide 'init)
+;;; init.el ends here
 
